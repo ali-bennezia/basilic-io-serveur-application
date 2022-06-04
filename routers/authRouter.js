58e471cbfed7aee9648a7aad8c4f48acbc7509ea
ali-bennezia <bennezia.ali@gmail.com>
2022-06-04 04:43:01 +0200

    Ajout du point POST /api/auth/token/authentify

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//Contrôleurs.
+const authController = require("./../controllers/authController");
+
 // POST /api/auth/token/authentify
 router.post("/token/authentify");
 
